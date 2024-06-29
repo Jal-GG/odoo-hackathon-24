fce2bf24b86b3c1c9b0aa151d879256b7f00e5d8
smit41103 <96989384+smit41103@users.noreply.github.com>
2024-06-29 14:10:32 +0530

    login ready

--- backend/database/connectDB.js
+++ backend/database/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () =>{
     try {
-        await mongoose.connect("mongodb+srv://cluster0:cZt4uZRyLlaxwXuQ@cluster0.iv50o9y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect("mongodb+srv://kalp:FDHpO3YxDYihJHrZ@chatapp.jwuw7sf.mongodb.net/?appName=chatapp");
         console.log("connected to MongoDB");
     }
     catch(err){
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import AuthRoutes from './routes/AuthRoutes.js';
-import cookieParser from 'cookie-parser';
-import connectDB from './database/connectDB.js';
+import express from "express";
+import dotenv from "dotenv";
+import AuthRoutes from "./routes/AuthRoutes.js";
+import cookieParser from "cookie-parser";
+import connectDB from "./database/connectDB.js";
 
 dotenv.config();
 
@@ -12,21 +12,15 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-// Connect to MongoDB
-// const run = async () => {
-//   await connectDB();
-// }
-
-
 // Use auth routes
-app.use('/api', AuthRoutes);
+app.use("/api", AuthRoutes);
 
 // Start the server
 app.listen(port, () => {
   connectDB();
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
